4f08e50aa432697265b9fe200dead53c414921f8
Kevin Sun <kevinsun242@gmail.com>
2019-01-25 20:29:16 -0500

    Added conversion to postfix

--- lib/practice/calc2.ex
+++ lib/practice/calc2.ex
@@ -10,6 +10,7 @@ defmodule Practice.Calc do
     expr
     |> String.split(~r/\s+/)
     |> tag_tokens
+    |> to_postfix
   end
 
   def tag_tokens(expr) do
@@ -23,6 +24,51 @@ defmodule Practice.Calc do
       end)
   end
  
+  def to_postfix(expr) do
+    to_postfix_helper(expr, [], [])
+  end
+
+  def to_postfix_helper(expr, postfix, operators) do
+    cond do
+      length(expr) == 0 && length(operators) == 0 ->
+        postfix
+      length(expr) == 0 && length(operators) > 0 ->
+        pop_operator(expr, postfix, operators)
+      true ->
+        process_expr(hd(expr), expr, postfix, operators)
+    end
+  end
+
+  def process_expr({:num, x}, expr, postfix, operators) do
+    to_postfix_helper(tl(expr), postfix ++ [{:num, x}], operators)
+  end
+
+  def process_expr({:op, x}, expr, postfix, operators) do
+    to_stack_operator(expr, postfix, operators)  
+  end
+
+  def to_stack_operator(expr, postfix, operators) do
+    cond do
+      length(operators) == 0 ->
+        to_postfix_helper(tl(expr), postfix, operators ++ [hd(expr)])
+      hd(expr) == {:op, "+"} || hd(expr) == {:op, "-"} ->
+        pop_operator(expr, postfix, operators)
+      hd(expr) == {:op, "*"} && Enum.take(operators, -1) == {:op, "*"} ->
+        pop_operator(expr, postfix, operators)
+      hd(expr) == {:op, "*"} && Enum.take(operators, -1) == {:op, "/"} ->
+        pop_operator(expr, postfix, operators)
+      hd(expr) == {:op, "/"} && Enum.take(operators, -1) == {:op, "*"} ->
+        pop_operator(expr, postfix, operators)
+      hd(expr) == {:op, "/"} && Enum.take(operators, -1) == {:op, "/"} ->
+        pop_operator(expr, postfix, operators)
+      true ->
+        to_postfix_helper(tl(expr), postfix, operators ++ [hd(expr)])
+    end
+  end
+
+  def pop_operator(expr, postfix, operators) do 
+    to_postfix_helper(expr, postfix ++ [Enum.take((operators), -1)], Enum.drop(operators, -1))
+  end
 
   
     # Hint:
