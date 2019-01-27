619b508842b9974ac0f9ea336f098964d789634b
Kevin Sun <kevinsun@Kevins-MacBook-Pro.local>
2019-01-26 22:51:35 -0500

    Added comments to calc

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -15,6 +15,8 @@ defmodule Practice.Calc do
     |> evaluate
   end
 
+  # tag_tokens: turns each character into a tuple with either 
+  # {:num} or {:op} identifying it as a number of operator
   def tag_tokens(expr) do
     Enum.map(expr, fn x -> 
       cond do 
@@ -26,10 +28,15 @@ defmodule Practice.Calc do
       end)
   end
  
+  # to_postfix: convert the string into postfix notation
   def to_postfix(expr) do
     to_postfix_helper(expr, [], [])
   end
 
+  # to_postfix_helper: 
+  # expr is the current expression
+  # postfix is the postfix expression 
+  # operators is the stack of operators to be appended to postfix
   def to_postfix_helper(expr, postfix, operators) do
     cond do
       length(expr) == 0 && length(operators) == 0 ->
@@ -41,14 +48,23 @@ defmodule Practice.Calc do
     end
   end
 
+  # process_expr: If the next character is a number, append it to
+  # the postfix expression
   def process_expr({:num, x}, expr, postfix, operators) do
     to_postfix_helper(tl(expr), postfix ++ [{:num, x}], operators)
   end
 
+  # process_expr: If the next character is an operator, push it
+  # to the stack of operators
   def process_expr({:op, _}, expr, postfix, operators) do
     to_stack_operator(expr, postfix, operators)  
   end
 
+  # to_stack_operator: Push the operator to the stack of operators.
+  # operators cannot be in front of another operator of the same 
+  # or higher priority, so those need to be popped from the stack and
+  # appended to the postfix expression before the current operator can
+  # be pushed.
   def to_stack_operator(expr, postfix, operators) do
     cond do
       length(operators) == 0 ->
@@ -68,18 +84,24 @@ defmodule Practice.Calc do
     end
   end
 
+  # pop_operator: pop an operator from the stack and append to postfix
+  # the last operator pushed to stack is popped first
   def pop_operator(expr, postfix, operators) do 
     to_postfix_helper(expr, 
                       postfix ++ [Enum.at(Enum.take((operators), -1), 0)], 
                       Enum.drop(operators, -1))
   end
 
-  # Convert to prefix notation 
+  # to_prefix: Convert to prefix notation 
   # Since there are no parens, you can simply reverse postfix
   def to_prefix(expr) do
     Enum.reverse(expr)
   end
 
+  # evaluate: evaluate the prefix expression
+  # if the length of the expression is 1, then it is just a number
+  # so return that number
+  # Prefix is evaluated by scanning the expressing backwards
   def evaluate(expr) do 
     cond do
       length(expr) == 1 ->
@@ -89,14 +111,21 @@ defmodule Practice.Calc do
     end
   end
   
+  # evaluate_helper: if the expression is empty, then no more operations
+  # are needed so return the value
   def evaluate_helper(x, _, stack) when x == nil do 
     Enum.at(stack, 0)
   end
 
+  # evaluate_helper: if the next character is a nunmber, push it to the
+  # stack
   def evaluate_helper({:num, x}, expr, stack) do
     evaluate_helper(Enum.at(expr, -2), Enum.drop(expr, -1), stack ++ [x])
   end 
-
+  
+  # evaluate_helper: if the next character is an operator, pop the last two
+  # numbers pushed to the stack, evaluate them (with the last pushed number)
+  # on the right hand side, then push the new value to the stack
   def evaluate_helper({:op, operator}, expr, stack) do
     cond do
       length(stack) == 1 ->
